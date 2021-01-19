379149f934bbb2c962e4f2c7e5fde813202c2add
reevesalic <reevesalic@gmail.com>
2021-01-19 10:47:45 -0500

    commit tree 910763212d1f23d7bf056364a1bdf2eb20a5908d

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  
+  fetch("http://localhost:3000/toys")
+    .then(r => r.json())
+    .then(toys => {
+
+    let toysHTML = toys.map(function(toy){
+      return `
+      <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    </div>
+    `
+    })
+    console.log(toysHTML)
+  })
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
